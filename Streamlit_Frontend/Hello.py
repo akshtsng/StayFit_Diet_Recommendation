bbab4906b1ad22bfbfeb73ed6ea546dc95182739
Akshat Singh <akshtsng@gmail.com>
2024-05-23 23:31:22 +0530

    Update Hello.py

--- Streamlit_Frontend/Hello.py
+++ Streamlit_Frontend/Hello.py
@@ -15,3 +15,8 @@ st.markdown(
     Made by Akshat Singh, Baibhav Chel, Prathik P, R Sambhavi
     """
 )
+
+if st.button("Diet Recommendation"):
+    js = "window.open('https://diet-recommendation-system.streamlit.app/Diet_Recommendation', '_blank')"
+    html = '<script>{}</script>'.format(js)
+    st.markdown(html, unsafe_allow_html=True)
