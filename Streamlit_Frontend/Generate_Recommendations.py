5f2f4b64d3bf0ca0fbabea6c1860e1434b05da69
Akshat Singh <akshtsng@gmail.com>
2024-05-23 23:25:06 +0530

    Update Generate_Recommendations.py

--- Streamlit_Frontend/Generate_Recommendations.py
+++ Streamlit_Frontend/Generate_Recommendations.py
@@ -1,22 +1,10 @@
-import requests
-import json
+import streamlit as st
 
-class Generator:
-    def __init__(self,nutrition_input:list,ingredients:list=[],params:dict={'n_neighbors':5,'return_distance':False}):
-        self.nutrition_input=nutrition_input
-        self.ingredients=ingredients
-        self.params=params
+# Streamlit code to create the interface
+st.title("Diet Recommendation System")
 
-    def set_request(self,nutrition_input:list,ingredients:list,params:dict):
-        self.nutrition_input=nutrition_input
-        self.ingredients=ingredients
-        self.params=params
-
-    def generate(self,):
-        request={
-            'nutrition_input':self.nutrition_input,
-            'ingredients':self.ingredients,
-            'params':self.params
-        }
-        response=requests.post(url='http://localhost:8080/predict/',data=json.dumps(request))
-        return response
+# Button to generate recommendations
+if st.button("Generate Recommendations"):
+    js = "window.location.href = 'https://diet-recommendation-system.streamlit.app/Diet_Recommendation';"
+    html = '<script>{}</script>'.format(js)
+    st.markdown(html, unsafe_allow_html=True)
